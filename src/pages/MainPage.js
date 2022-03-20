981d7fe426ae63b4045edcf7547e3d9e8083b504
onggi <hsk1533@naver.com>
2022-03-20 16:42:22 +0900

    init

---
+++ src/pages/MainPage.js
@@ -0,0 +1,12 @@
+import "../css/MainPage.css";
+import MenuBar from "../utils/MenuBar";
+
+function MainPage() {
+  return (
+    <div className="main_page">
+      <MenuBar></MenuBar>
+    </div>
+  );
+}
+
+export default MainPage;
