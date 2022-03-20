c11e82c629eb9576d0c40a9d84b270a200cfcc20
onggi <hsk1533@naver.com>
2022-03-20 17:38:26 +0900

    menu bar design

--- src/utils/MenuBar.js
+++ src/utils/MenuBar.js
@@ -1,5 +1,24 @@
+import "../css/MenuBar.css";
+
 function MenuBar() {
-  return <div className="menu-bar"></div>;
+  return (
+    <div className="menu-bar">
+      <a className="name-tag" href="/">
+        <text>Hong Seunggi</text>
+      </a>
+      <div className="menu-bar-detail">
+        <a href="/recode">
+          <text>Recode</text>
+        </a>
+        <a href="/project">
+          <text>Project</text>
+        </a>
+        <a href="/about">
+          <text>About</text>
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default MenuBar;
