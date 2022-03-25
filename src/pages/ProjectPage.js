dc346018071536c79516c33b79f5e44892ac0551
onggi <hsk1533@naver.com>
2022-03-25 18:18:56 +0900

    api data error test

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,10 +1,11 @@
 import { motion } from "framer-motion";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Gitrepo from "../utils/Gitrepo";
 import axios from "axios";
 import "../css/ProjectPage.css";
 
 function ProjectPage() {
-  const datas = [];
+  const [datas, setDatas] = useState([]);
   const url = "https://api.github.com/users/seunggihong/repos";
 
   useEffect(() => {
@@ -12,11 +13,16 @@ function ProjectPage() {
       .get(url)
       .then((Response) => {
         for (let i = 0; i < Response.data.length; i++) {
-          const newData = {
-            name: Response.data[i].name,
-            htmlUrl: Response.data[i].html_url,
-          };
-          datas.push(newData);
+          const newData = (
+            <Gitrepo
+              key={Response.data[i].id}
+              htmlUrl={Response.data[i].html_url}
+              name={Response.data[0].name}
+            />
+          );
+          let newDatas = [...datas];
+          newDatas.push(newData);
+          setDatas(newDatas);
         }
       })
       .catch((err) => {
@@ -30,8 +36,9 @@ function ProjectPage() {
       animate={{ opacity: 1 }}
       transition={{ duration: 2 }}
     >
+      {console.log(datas)}
       <div className="project-frame">
-        <canvas className="project-frame-canvas"></canvas>
+        <div className="project-frame-div"></div>
       </div>
     </motion.div>
   );
--- src/utils/Gitrepo.js
+++ src/utils/Gitrepo.js
@@ -3,7 +3,9 @@ import "../css/Gitrepo.css";
 function Gitrepo(props) {
   return (
     <div className="git-repo-frame">
-      <a href={props.htmlUrl}>{props.name}</a>
+      <a key={props.key} href={props.htmlUrl}>
+        {props.name}
+      </a>
     </div>
   );
 }
