a3bb53b5d2239379e178f09c85e4fcc7e0e25fd0
onggi <hsk1533@naver.com>
2022-04-05 16:21:14 +0900

    about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,4 +1,6 @@
 import { motion } from "framer-motion";
+import { FaInstagram, FaGithub } from "react-icons/fa";
+import "../css/AboutPage.css";
 
 function AboutPage() {
   return (
@@ -7,7 +9,17 @@ function AboutPage() {
       animate={{ opacity: 1 }}
       transition={{ duration: 2 }}
     >
-      <h1>About Page</h1>
+      <div className="about_frame">
+        <h1>Made by Hong Seunggi</h1>
+        <div className="about_frame_icon">
+          <a href="https://github.com/seunggihong" target="blank">
+            <FaGithub size={30} className="fa-github" />
+          </a>
+          <a href="https://www.instagram.com/iggnuxs/" target="blank">
+            <FaInstagram size={30} className="fa-insta" />
+          </a>
+        </div>
+      </div>
     </motion.div>
   );
 }
