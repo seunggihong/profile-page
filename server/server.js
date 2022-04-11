883aac901c80ed8b5fd8c70322f3ba467d46ce25
onggi <hsk1533@naver.com>
2022-04-11 17:49:50 +0900

    resolve routing error in server

--- server/server.js
+++ server/server.js
@@ -5,11 +5,14 @@ const app = express();
 
 const http = require("http").createServer(app);
 
-http.listen(3001, () => {
-  console.log("listening on 3001 port");
+http.listen(3000, () => {
+  console.log("listening on 3000 port");
 });
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
